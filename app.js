ed3b21930d8fd8a570d3940dd3e02ee90071bac0
RabarS13 <Rabar_s@ymail.com>
2025-05-13 14:54:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'front.html'));
 });
 
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
